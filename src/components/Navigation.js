3b8f22595720773a0e71dffddea4e6939dd7db64
kitkatmia <alinka.markova@gmail.com>
2022-10-20 20:05:11 -0700

    minor changes to styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { StaticImage } from "gatsby-plugin-image";
 
 const Navigation = () => {
     return (
         <nav>
+            {/* <StaticImage
+                src="../images/MVHacksLogoMV.png"
+                loading="eager"
+                height={5}
+                width={5}
+                quality={95}
+                alt="MV Hacks logo"
+                style={{ margin: 0 }}
+            /> */}
             <h1>MVHacks</h1>
             <div className='links'>
                 {/* in vanilla js, you may use <a/> tag, but <Link/> is more efficient in this case because it keeps everything on the same page and so is faster */}
-                <Link to="">
+                <Link to="/">
                     Home
                 </Link>
                 <Link to="/page-2">

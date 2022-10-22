48b4bbf6bbe46bb296f0bbec080a01bd9c48624f
kitkatmia <alinka.markova@gmail.com>
2022-10-21 20:31:40 -0700

    fixed navigation header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,7 @@ import { StaticImage } from "gatsby-plugin-image";
 const Navigation = () => {
     return (
         <nav>
+            {/* need to add static image logo to nav bar as header instead of using text */}
             {/* <StaticImage
                 src="../images/MVHacksLogoMV.png"
                 loading="eager"
@@ -14,7 +15,7 @@ const Navigation = () => {
                 alt="MV Hacks logo"
                 style={{ margin: 0 }}
             /> */}
-            <h1>MVHacks</h1>
+            <Link to="/" id='title' ReachLink>MV Hacks</Link>
             <div className='links'>
                 {/* in vanilla js, you may use <a/> tag, but <Link/> is more efficient in this case because it keeps everything on the same page and so is faster */}
                 <Link to="/">

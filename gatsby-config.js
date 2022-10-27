0bdce7fcd48e2db7472bc80e461e9c73e7e8dd69
kitkatmia <alinka.markova@gmail.com>
2022-10-27 08:57:31 -0700

    mvhacks logo on appears on tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `MV Hacks 6.0`,
+    description: `The hackathon for everyone. Especially beginners. Apply today.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
     // menuLinks: [
@@ -33,12 +33,12 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#663399`, // needs to be changed
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/MVHacksLogoMV.png`, // This path is relative to the root of the site.
       },
     },
     {

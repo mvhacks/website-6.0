c4b53a19a78a9acd7dd29542db7e8b7a39367d0a
kitkatmia <alinka.markova@gmail.com>
2022-11-08 18:54:44 -0800

    added alternate theme

--- src/pages/about-the-team.js
+++ src/pages/about-the-team.js
@@ -8,6 +8,7 @@ import { themeObj } from '../utils/customTheme';
 
 const AboutTheTeam = () => (
     <Layout>
+        <h1 style={{ color: themeObj.dark.palette.secondary.main }}>Hi.</h1>
         <h1 className="titleStyle" style={{ color: themeObj.dark.palette.secondary.main }}>this page isn't done yet. please go away.</h1>
     </Layout >
 );

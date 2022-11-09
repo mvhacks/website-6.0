005d7985809989c6bc16e6f7ab200ff41cefbfc5
kitkatmia <alinka.markova@gmail.com>
2022-11-08 20:17:39 -0800

    display image + name for team page

--- src/pages/about-the-team.js
+++ src/pages/about-the-team.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import "./about-the-team.scss";
 
+import people from "../teamInfo/teamInfo.json";
+
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
@@ -10,6 +12,19 @@ const AboutTheTeam = () => (
     <Layout>
         <h1 style={{ color: themeObj.dark.palette.secondary.main }}>Hi.</h1>
         <h1 className="titleStyle" style={{ color: themeObj.dark.palette.secondary.main }}>this page isn't done yet. please go away.</h1>
+        <div className="inner" key={people.name}>
+            {
+                people && people.map(person => {
+                    return (
+                        <div className="box" style={{ backgroundColor: themeObj.dark.palette.secondary.main, color: themeObj.dark.palette.accent.main }}>
+                            <img src={person.imgSrc} style={{ aspectRatio: "1/1", height: "30vh" }} alt='profile' />
+                            <h3>{person.name}</h3>
+                        </div>
+                    )
+                })
+            }
+
+        </div>
     </Layout >
 );
 

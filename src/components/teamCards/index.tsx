65678fb0c1ac5268dad42cee7afaa70b2fd45b72
kitkatmia <alinka.markova@gmail.com>
2022-11-26 12:44:39 -0800

    about the team generally done

--- src/components/teamCards/index.tsx
+++ src/components/teamCards/index.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { themeObj } from '../../utils/customTheme';
 import './styles.scss';
 
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box
+} from '@chakra-ui/react'
+
 interface Person {
     name: string,
     title: string,
@@ -19,46 +28,52 @@ interface Props {
     team: String;
 }
 
-// people = {[efief: je, iefeoif:efew, etc]}
 const TeamCards = ({ people, team }: Props) => {
-
     return (
-    <>
-    <h1  style={{
-                textAlign: "left",
-                color: themeObj.dark.palette.secondary.main,
-                marginTop: "10vh",
-                marginBottom: "5vh",
-                marginLeft: "3vh",
-                fontSize: "56px",
-                fontWeight: "300",
-            }}
-            >{team}</h1>
-    <div className="inner">
-      {people &&
-        people.map((person, index) => (
-          <div
-            key={`person-${index}`}
-            className="box"
-            style={{
-                backgroundColor: themeObj.dark.palette.secondary.main,
-                color: themeObj.dark.palette.accent.main,
-                outlineColor: person.isDirector ? themeObj.dark.palette.secondary.secondary: themeObj.dark.palette.primary.main,
-                // E2FCEF
-                outlineWidth: "5px",
-                outlineStyle: "double"
-            }}
-          >
-            <img
-              src={person.imgSrc}
-              style={{ aspectRatio: '1/1', height: '30vh' }}
-              alt="profile"
-                />
-                <br />
-            <h3>{person.name}</h3>
+      <>
+      <AccordionItem marginBottom="10vh" borderStyle="none">
+        <h2 style={{color: themeObj.dark.palette.secondary.main}}>
+          <AccordionButton style={{width: "30%", marginBottom: "10vh", outline: "4px inset", outlineColor: themeObj.dark.palette.secondary.main}}  _disabled={{background: themeObj.dark.palette.secondary.main, color: themeObj.dark.palette.primary.main}} _expanded={{ bg: themeObj.dark.palette.secondary.main, color: themeObj.dark.palette.primary.main }}>
+              <Box style={{    
+              textAlign: "left",
+              fontSize: "3.5vw",
+              fontWeight: "300",
+              marginRight: "5vh",
+              outlineColor: themeObj.dark.palette.secondary.main,
+            }}>
+            {team}
+            </Box>
+            <AccordionIcon style={{scale: "150%"}}/>
+          </AccordionButton>
+        </h2>
+        <AccordionPanel>
+          <div className="inner">
+            {people &&
+              people.map((person, index) => (
+                <div
+                  key={`person-${index}`}
+                  className="box"
+                  style={{
+                      backgroundColor: themeObj.dark.palette.secondary.main,
+                      color: themeObj.dark.palette.accent.main,
+                      outlineColor: person.isDirector ? themeObj.dark.palette.secondary.secondary: themeObj.dark.palette.primary.main,
+                      // E2FCEF
+                      outlineWidth: "5px",
+                      outlineStyle: "double"
+                  }}
+                >
+                  <img
+                    src={person.imgSrc}
+                    style={{ aspectRatio: '1/1', height: '30vh' }}
+                    alt="profile"
+                      />
+                      <br />
+                  <h3>{person.name}</h3>
+                </div>
+              ))}
           </div>
-        ))}
-    </div>
+        </AccordionPanel>
+      </AccordionItem>
     </>
   );
 };
--- src/pages/about-the-team.tsx
+++ src/pages/about-the-team.tsx
@@ -9,24 +9,32 @@ import Seo from '../components/seo';
 import { themeObj } from '../utils/customTheme';
 import TeamCards from '../components/teamCards';
 
+import {
+  Accordion,
+} from '@chakra-ui/react'
+
 const leadTeam = people.filter(a => a.isDirector == true);
 const techTeam = people.filter(a => a.team == "tech");
 const eventTeam = people.filter(a => a.team == "event");
 const outreachTeam = people.filter(a => a.team == "outreach");
-
+const t = "test";
 const AboutTheTeam = () => (
   <Layout>
     <div>
       <h1
       className="titleStyle"
-      style={{ color: themeObj.dark.palette.secondary.main }}
+      style={{ color: themeObj.dark.palette.secondary.main, marginBottom: "4vh" }}
       >
       About the team!
       <br />
       </h1>
     </div> 
-    <TeamCards people={leadTeam} team={"Team Leads"} />
-    <TeamCards people={techTeam} team={"Tech Team"} />
+    <Accordion defaultIndex={[0]} allowToggle allowMultiple style={{ borderStyle: "none" }}> 
+      <TeamCards people={leadTeam} team="Team Leads" />
+      <TeamCards people={techTeam} team="Tech Team" />
+      <TeamCards people={eventTeam} team="Event Team" />
+      <TeamCards people={outreachTeam} team="Outreach Team" />
+    </Accordion>
   </Layout>
 );
 

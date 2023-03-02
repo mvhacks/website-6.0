b35a8d1e107fee8f1414c7fe4f32e70c3e54758c
kitkatmia <alinka.markova@gmail.com>
2023-03-02 07:43:02 -0800

    minor changes

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -8,16 +8,7 @@ import theme from '../utils/customTheme';
 import { themeObj } from '../utils/customTheme';
 import sponsorInfo from '../images/sponsorInfo.png';
 
-
-
-// import {useState} from 'react';
-
 const SponsorsPage = () => {
-    // const data = [
-    //     { id: 1, name: "John", age: 30 },
-    //     { id: 2, name: "ko", age: 15 },
-    //     { id: 3, name: "k", age: 21 }
-    // ];
   return (
     <ChakraProvider theme={theme}>
       <Layout>
@@ -30,32 +21,13 @@ const SponsorsPage = () => {
                 className="titleStyle"
                 style={{ color: themeObj.dark.palette.secondary.main, fontSize: "5vw" }}
             >
-                Sponsors
+                Sponsor us!
             </h1>
-                  
-            {/* <table style={{color:themeObj.dark.palette.accent.main}}>
-                <thead style={{color:themeObj.dark.palette.accent.main}}>
-                    <tr style={{color:themeObj.dark.palette.accent.main}}>
-                        <th style={{color:themeObj.dark.palette.accent.main}}>ID</th>
-                        <th style={{color:themeObj.dark.palette.accent.main}}>Name</th>
-                        <th style={{color:themeObj.dark.palette.accent.main}}>Age</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map(item => (
-                        <tr style={{color:themeObj.dark.palette.accent.main}}> 
-                            <td key={item.id} style={{color:themeObj.dark.palette.accent.main}}></td>
-                            <td key={item.name} style={{color:themeObj.dark.palette.accent.main}}></td>
-                            <td key={item.age} style={{color:themeObj.dark.palette.accent.main}}></td>
-                        </tr>
-                    ))}
-                </tbody>   
-            </table> */}
             <div style={{display:"flex", justifyContent:"center", alignItems: "center", margin:"10vh"}}>
                 <img src={sponsorInfo} alt="Sponsor Info" style={{maxWidth:"90%"}}/>
             </div>
 
-            <a href="https://drive.google.com/drive/folders/1ljtLf6Iww6sjgUNMTVPjhE74XMIhMZjP" target="_blank" style={{color:themeObj.dark.palette.accent.main}}> Prospectus</a>  
+            <a href="https://drive.google.com/file/d/1FyM05z4O_me3PSV0WxpTatO3uiGxedWk/view?usp=sharing" target="_blank" style={{color:themeObj.dark.palette.accent.main, display:"flex", justifyContent:"center", alignItems: "center", padding:"5vh"}}>Click to see prospectus</a>  
         </div>
       </Layout>
     </ChakraProvider>
